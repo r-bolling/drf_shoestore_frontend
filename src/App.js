7d14fb89cd6d9ddab243e29ce91fa6771475bb9f
Robert Bolling <robboles1014@gmail.com>
2021-01-02 12:17:40 -0500

    Displayed shoe data from backend on page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [shoes, setShoes] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8000/api/shoe')
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setShoes(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <ul>
+        {shoes.map(shoe => (
+          <li key={shoe.id}>
+            {shoe.size}
+            <br />
+            {shoe.brand_name}
+            <br />
+            {shoe.manufacturer}
+            <br />
+            {shoe.color}
+            <br />
+            {shoe.material}
+            <br />
+            {shoe.shoe_type}
+            <br />
+            {shoe.fasten_type}
+            <br />
+            <br />
+          </li>
+          
+        ))}
+      </ul>
+    )
+  }
 }
 
 export default App;
